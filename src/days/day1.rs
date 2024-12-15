8ee1ba86400ac31766a408d71e510d59ce582318
Eli Uriegas <github@terriblecode.com>
2024-12-15 15:47:54 -0800

    solve for the second part
    Signed-off-by: Eli Uriegas <github@terriblecode.com>

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,7 +1,8 @@
 use clap::Args;
 
-use itertools::{interleave, Itertools};
-use log::{debug, info};
+use itertools::Itertools;
+use log::info;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
@@ -31,23 +32,73 @@ fn read_input_file(path: &str) -> Result<(Vec<i32>, Vec<i32>), io::Error> {
     return Ok((list1, list2));
 }
 
-fn sort_vector_with_positions(input_vec: &Vec<i32>) -> Vec<i32> {
+fn sort_vector(input_vec: &Vec<i32>) -> Vec<i32> {
     return input_vec.iter().cloned().sorted().collect();
+}
+
+fn compute_total_distance(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
+    let sorted_list1 = sort_vector(list1);
+    let sorted_list2 = sort_vector(list2);
+    let mut total_distance = 0;
+    let mut i = 0;
+    while i < sorted_list1.len() {
+        total_distance += (sorted_list1[i] - sorted_list2[i]).abs();
+        i += 1;
+    }
+    return total_distance;
+}
+
+fn compute_similarity_score(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
+    let mut occurence_map: HashMap<i32, i32> = HashMap::new();
+    let mut similarity_score = 0;
+
+    for x in list2.iter() {
+        if occurence_map.contains_key(&x) {
+            occurence_map.insert(*x, occurence_map[&x] + 1);
+        } else {
+            occurence_map.insert(*x, 1);
+        }
+    }
+    for key in list1 {
+        if occurence_map.contains_key(&key) {
+            similarity_score += key * occurence_map[&key];
+        }
+    }
+    return similarity_score;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sort_vector_with_positions() {
+        let input_vec = vec![3, 1, 2];
+        let expected_output = vec![1, 2, 3];
+        assert_eq!(sort_vector(&input_vec), expected_output);
+    }
+
+    #[test]
+    fn test_compute_total_distance() {
+        let list1 = vec![3, 4, 2, 1, 3, 3];
+        let list2 = vec![4, 3, 5, 3, 9, 3];
+        assert_eq!(compute_total_distance(&list1, &list2), 11);
+    }
+
+    #[test]
+    fn test_compute_similarity_score() {
+        let list1 = vec![3, 4, 2, 1, 3, 3];
+        let list2 = vec![4, 3, 5, 3, 9, 3];
+        assert_eq!(compute_similarity_score(&list1, &list2), 31);
+    }
 }
 
 impl Day1Command {
     pub fn execute(&self) {
-        let inputs = read_input_file(&self.input_file).ok();
-        inputs.map(|(list1, list2)| {
-            let sorted_list1 = sort_vector_with_positions(&list1);
-            let sorted_list2 = sort_vector_with_positions(&list2);
-            let mut total_distance = 0;
-            let mut i = 0;
-            while i < sorted_list1.len() {
-                total_distance += (sorted_list1[i] - sorted_list2[i]).abs();
-                i += 1;
-            }
-            info!("Total distance: {}", total_distance);
-        });
+        let (list1, list2) = read_input_file(&self.input_file).ok().unwrap();
+        let total_distance = compute_total_distance(&list1, &list2);
+        let similarity_score = compute_similarity_score(&list1, &list2);
+        info!("Total distance: {}", total_distance);
+        info!("Similarity score: {}", similarity_score);
     }
 }
